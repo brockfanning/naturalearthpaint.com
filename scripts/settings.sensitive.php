be2fc09c3857632df6ed154b48f3a9a5d8f170d1
Brock Fanning <brockfanning@gmail.com>
2017-12-28 09:13:18 -0500

    Braintree config example and update

---
+++ scripts/settings.sensitive.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Example settings.php data for sensitive info.
+ *
+ * This unusued file is to demonstrate the data that needs to be copied into
+ * settings.php and replaced with actual values.
+ */
+
+// API info for Braintree integration.
+$conf['commerce_payment.commerce_payment_gateway.braintree']['configuration']['merchant_id'] = 'REPLACEME';
+$conf['commerce_payment.commerce_payment_gateway.braintree']['configuration']['public_key'] = 'REPLACEME';
+$conf['commerce_payment.commerce_payment_gateway.braintree']['configuration']['private_key'] = 'REPLACEME';
+$conf['commerce_payment.commerce_payment_gateway.braintree']['configuration']['merchant_account_id']['USD'] = 'REPLACEME';
